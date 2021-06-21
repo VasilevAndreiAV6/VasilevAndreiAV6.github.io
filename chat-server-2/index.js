f09065e06d3528d290873682b921543d302ac4bf
VasilevAndreiAV6 <68250467+VasilevAndreiAV6@users.noreply.github.com>
2021-06-21 16:54:21 +0300

    Add files via upload

---
+++ chat-server-2/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+let messages = [];
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    if (JSON.parse(msg).name != "" && JSON.parse(msg).message != "") {
+      messages.push(JSON.parse(msg));
+      console.log('message: ' + JSON.stringify(messages));
+    }
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', JSON.stringify(messages));
+    console.log("andrey lox.\n FLEXXXX");
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on: 3000');
+});
